fab8277f650109c552a3df77bc93f6f0356191ab
Jolly Walker <wz.lim.00@gmail.com>
2026-05-26 17:21:36 +0800

    chore: fmt

--- src/Interface/IAsterDex.sol
+++ src/Interface/IAsterDex.sol
@@ -164,7 +164,6 @@ interface IAsterDex {
     );
 }
 
-
 interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 amount) external returns (bool);
